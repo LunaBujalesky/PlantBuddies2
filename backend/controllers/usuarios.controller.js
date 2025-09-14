a62b50a067c418bd3199b432731105607b99c0e7
romina-iurchik <romina-iurchik@github.com>
2025-09-13 21:22:43 -0300

    Agrega validaciones al backend y mejoras en el frontend

--- backend/controllers/usuarios.controller.js
+++ backend/controllers/usuarios.controller.js
@@ -1,7 +1,38 @@
 exports.crearUsuario = (req, res) => {
     const { nombre, email, pais, ciudad } = req.body;
+    //valido del lado del backend
+    //funciones para validar
+    const campoVacio = (valor) => !valor || valor.trim() === '';
 
-    console.log('Usuario recibido: ', req.body);
+    const emailValidado = (email) => {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return regex.test(email);
+    };
 
-    res.status(200).json({mensaje: 'Usuario recibido correctamente'});
+    //Validaciones
+    if(campoVacio(nombre)){
+        return res.status(400).json({error: '⛔ Nombre obligatorio'});
+    }
+    
+    if(campoVacio(email)){
+        return res.status(400).json({error: '⛔ Correo electrónico obligatorio'});
+    }
+    if(!emailValidado(email)){
+        return res.status(400).json({error: '📧 El mail no es valido'});
+    }
+
+    if(campoVacio(pais)){
+        return res.status(400).json({error: '⛔ Pais obligatorio'});
+    }
+
+        if(campoVacio(ciudad)){
+        return res.status(400).json({error: '⛔ Ciudad obligatorio'});
+    }
+    
+    //si pasa todas las validaciones
+
+    res.json({
+        mensaje: '👱🏻 Usuario recibido correctamente',
+        datos: req.body
+    });
 }
